ec721d317c7cf057cf7bd040f078baeccc061839
saqib khan <saqibpathan776@gmail.com>
2023-09-26 18:52:11 +0530

    Version 1.0

---
+++ menu.js
@@ -0,0 +1,10 @@
+
+
+
+
+function onClickMenu(){
+document.getElementById("menu").classList.toggle("icon")
+document.getElementById("nav").classList.toggle("change")
+document.getElementById("nav-1").classList.toggle("hello")
+// document.getElementById("form-footer-bottom").classList.toggle("footer-bottom")
+}
